853de1bea639353b2265af23c86880cdb805a4a7
Maribel Montes <maribel.doris@gmail.com>
2022-01-29 00:24:07 -0600

    added the questions to be used on the prompt and a sample of the layout of the readme

--- Develop/index.js
+++ Develop/index.js
@@ -13,3 +13,14 @@ function init() {}
 init();
 
 // git@github.com:MaryMD98/ReadMe-generator.git
+// here are some of the command promt questions
+// What is your GitHub username?
+// What is your email address?
+// What is your project's name?
+// Please write a short description of your project.
+// What kind of license should your project have? ** multiple choice
+//** MIT APACHE BSD NONE */
+// what command should be run to install dependencies?
+// what command should be run to run tests?
+// what does the user need to know about using the repo?
+// what does the user need to know about contributing to the repo?
---
+++ Develop/read.md
@@ -0,0 +1,12 @@
+# Title:
+## Description:
+
+## Table of Content:
+
+## Installation:
+To run tests, run the following command:
+box needs to dsplay with comand
+
+## Questions:
+
+If you have any questions about the repo, open an issue or would like to contact me directly at email**beFiled. You cn find more of my work at [Maribel Montes](https://github.com/REPLACEMaryMD98).
