9a2b2070afd02e918bcb29595bb967f6160060c4
Maribel Montes <maribel.doris@gmail.com>
2022-01-30 03:32:16 -0600

    added code to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -18,7 +18,10 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,generateREADme(data),
+    (error) => error ? console.error(error): console.log("Generating README...."))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -26,15 +29,7 @@ function init() {}
 // Function call to initialize app
 init();
 
-// git@github.com:MaryMD98/ReadMe-generator.git
-// here are some of the command promt questions
-// What is your GitHub username?
-// What is your email address?
-// What is your project's name?
-// Please write a short description of your project.
-// What kind of license should your project have? ** multiple choice
-//** MIT APACHE BSD NONE */
-// what command should be run to install dependencies?
-// what command should be run to run tests?
-// what does the user need to know about using the repo?
-// what does the user need to know about contributing to the repo?
+// function to write the readme
+var generateREADme = (data) =>{
+    return ` hello i am working ${data.hello}`
+}
